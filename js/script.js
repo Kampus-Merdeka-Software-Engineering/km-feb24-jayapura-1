ad0b492ab8fdab7ec4d56daefef5438b8de325c0
Naufal Hawari <naufalhawari7@gmail.com>
2024-05-17 23:35:16 +0700

    Perubahan agar sidebar responsif

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-
+function toggleSidebar() {
+    const stylesheet = document.getElementById('stylesheet');
+    if (stylesheet.getAttribute('href') === 'css/styles.css') {
+        stylesheet.setAttribute('href', 'css/styles2.css');
+    } else {
+        stylesheet.setAttribute('href', 'css/styles.css');
+    }
+}
